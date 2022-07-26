ef5dc4a1f23e7e01bd3c9abf55d07c2eb2bfdb7c
Mishael <somtookonkwo96.gmail.com>
2022-07-27 00:27:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Search from './components/Search';
+
+function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RAPIDAPI-KEY": "",
+    "X-RAPIDAPI-HOST": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
